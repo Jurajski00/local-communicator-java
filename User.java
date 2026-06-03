3b8b0901da17a911aa6faf3ad80099a4ddcde9b8
Daniel Jurzyk <daniel@Daniels-MacBook-Air.local>
2026-06-03 21:37:15 +0200

    chore(old logic): reomve message storing inside user class

--- User.java
+++ User.java
@@ -1,28 +1,14 @@
 package project;
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class User {
     private static int nextId = 1;
     private final int id;
     private String name;
-    private List<Message> messages = new ArrayList<>();
 
     public User(String name) {
         this.name = name;
         this.id = nextId;
         nextId++;
-    }
-
-    public void recieveMessage(Message message) {
-        messages.add(message);
-    }
-
-    public void getMessages() {
-        for(Message message : messages) {
-            System.out.println(message.getContent());
-        }
     }
 
     public String getName() {
